c62c72c96b557dd2a0b3ef51252b3cea4a645b98
klarski <kristennl1@gmail.com>
2014-10-30 00:13:21 -0400

    Add Practice files and week 1 project directory

---
+++ week1practice/main.py
@@ -0,0 +1,44 @@
+#one lined comments
+
+'''
+Doc Strings
+'''
+#variables
+first_name = "Kermit"
+last_name = "The Frog"
+
+#print first_name
+
+#response = raw_input("Enter your name: ")
+#print response
+
+#Expressions
+birth_year = 1987
+current_year = 2014
+age = current_year - birth_year
+#print "You are " + str(age) + " years old"
+
+#conditions
+
+budget = 90
+
+'''
+if budget > 100:
+    brand = "nike"
+    print "Yay, we can buy cool " + brand + " shoes"
+elif budget > 70:
+    print "We can at least get some generic sneakers"
+else:
+    pass
+'''
+#arrays
+characters = ["Abed", "Troy", "Annie", "Britta"]
+characters.append("Jeff")
+print characters[0]
+
+#dictonarys
+movies = dict()
+movies = {"Despicable Me":"Gru", "Little Mermaid":"Ariel"}
+
+#print movies["Despicable Me"]
+
