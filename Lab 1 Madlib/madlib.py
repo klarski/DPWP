a39bf705a7a5050a68541141e5479a79f5730488
klarski <kristennl1@gmail.com>
2014-10-30 22:45:47 -0400

    Add sports team function

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -6,18 +6,19 @@ Design Patterns for Web Programming
 
 madlib_answers = dict()
 
-#madlib_answers['name']= raw_input('Enter a name:  ')
-#madlib_answers['adjective1'] = raw_input('Enter an adjective: ')
-#madlib_answers['verb'] = raw_input('Enter a verb ending with ed: ')
-#madlib_answers['alcohol'] = raw_input('Enter the name of an alcohol: ')
-#madlib_answers['alcohol'] = raw_input('Enter the name of another alcohol: ')
-#madlib_answers['alcohol'] = raw_input('Enter the name of yet another alcohol: ')
+madlib_answers['name']= raw_input('Enter a name:  ')
+madlib_answers['adjective1'] = raw_input('Enter an adjective: ')
+madlib_answers['verb'] = raw_input('Enter a verb ending with ed: ')
+madlib_answers['alcohol'] = raw_input('Enter the name of an alcohol: ')
+madlib_answers['alcohol'] = raw_input('Enter the name of another alcohol: ')
+madlib_answers['alcohol'] = raw_input('Enter the name of yet another alcohol: ')
 madlib_answers['booknumber'] = raw_input('Enter a number: ')
-#madlib_answers['celebrity'] = raw_input('Enter the name of a celebrity: ')
-#madlib_answers['animal_number'] = raw_input('Enter a number: ')
-#madlib_answers['animal'] = raw_input('Enter an animal: ')
-#madlib_answers['daynumber'] = raw_input('Enter a number: ')
-#madlib_answers['adjective2'] = raw_input('Enter an adjective: ')
+madlib_answers['celebrity'] = raw_input('Enter the name of a celebrity: ')
+madlib_answers['animal_number'] = raw_input('Enter a number: ')
+madlib_answers['animal'] = raw_input('Enter an animal: ')
+madlib_answers['daynumber'] = raw_input('Enter a number: ')
+madlib_answers['adjective2'] = raw_input('Enter an adjective: ')
+madlib_answers['sports_team'] = raw_input('Choose a sports team: ')
 
 if madlib_answers['booknumber'] > 50:
     total_books = int(madlib_answers['booknumber'])*2
@@ -25,3 +26,8 @@ if madlib_answers['booknumber'] > 50:
 else:
     total_books = madlib_answers['booknumber']/2
     total_books = (str(total_books) + ' books in my life so I am not very bright.')
+
+def sport_team():
+    for i in range(1,3):
+        print "Go " + madlib_answers['sports_team'] + " Go!"
+        i = i + 1
