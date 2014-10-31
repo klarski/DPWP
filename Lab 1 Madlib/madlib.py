d34e1880e0995fb48d069815d6db0ca0b96591b5
klarski <kristennl1@gmail.com>
2014-10-30 23:20:53 -0400

    add about conditional

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -7,6 +7,8 @@ Design Patterns for Web Programming
 madlib_answers = dict()
 
 madlib_answers['name']= raw_input('Enter a name:  ')
+madlib_answers['gender'] = raw_input('Enter your gender (Male or Female):  ')
+madlib_answers['age'] = raw_input('Enter your age: ')
 madlib_answers['adjective1'] = raw_input('Enter an adjective: ')
 madlib_answers['verb'] = raw_input('Enter a verb ending with ed: ')
 madlib_answers['alcohol'] = raw_input('Enter the name of an alcohol: ')
@@ -20,6 +22,22 @@ madlib_answers['daynumber'] = raw_input('Enter a number: ')
 madlib_answers['adjective2'] = raw_input('Enter an adjective: ')
 madlib_answers['sports_team'] = raw_input('Choose a sports team: ')
 
+
+#conditional for about
+
+about = ""
+
+if madlib_answers['gender'] == "Male" and int(madlib_answers['age']) > 40:
+    about = "I am a mature male looking for companionship."
+elif madlib_answers['gender'] == "Male" and int(madlib_answers['age']) < 40:
+    about = "I am a vivacious young fella and not looking for anything serious."
+elif madlib_answers['gender'] == "Female" and int(madlib_answers['age']) > 40:
+    about = "I am a experienced older woman who likes younger men."
+else:
+    about = "I am a independent career oriented woman looking for someone who can keep up with my active life"
+
+
+#conditional statement that prints how many books you read and if you are smart
 if madlib_answers['booknumber'] > 50:
     total_books = int(madlib_answers['booknumber'])*2
     total_books = (str(total_books) + ' books in my life so I am very smart.')
@@ -27,7 +45,10 @@ else:
     total_books = madlib_answers['booknumber']/2
     total_books = (str(total_books) + ' books in my life so I am not very bright.')
 
+#function with loop inside of function the prints sports chant in personal ad
 def sport_team():
     for i in range(1,3):
         print "Go " + madlib_answers['sports_team'] + " Go!"
         i = i + 1
+
+
