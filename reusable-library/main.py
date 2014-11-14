1268576e2a08f8bea41e22d9bd1db2aeaf2f3f91
klarski <kristennl1@gmail.com>
2014-11-14 03:55:46 -0500

    add comments to main.py

--- reusable-library/main.py
+++ reusable-library/main.py
@@ -12,29 +12,29 @@ from lib import FormData, FormCal
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        f = FormPage()
-        data = FormData()
-        lib = FormCal()
-        results = ResultsPage()
+        f = FormPage()  # creating instance of FormPage class
+        data = FormData()  # creating instance of FormData class
+        lib = FormCal()  # creating instance of FormCal class
+        results = ResultsPage()  # creating instance of ResultsPage class
 
-        if self.request.GET:
-            data.fname = self.request.GET['fname']
-            data.lname = self.request.GET['lname']
-            data.dob = self.request.GET['dob']
-            data.username = self.request.GET['username']
-            data.password = self.request.GET['password']
-            data.ver_password = self.request.GET['verify-password']
-            data.robot = self.request.GET['robot']
+        if self.request.GET:  # this if statement checks to see if the form is filled out and then gets the answers
+            data.fname = self.request.GET['fname']  # first name input
+            data.lname = self.request.GET['lname']  # last name input
+            data.dob = self.request.GET['dob']  # Date of birth input
+            data.username = self.request.GET['username']  # username input
+            data.password = self.request.GET['password']  # password input
+            data.ver_password = self.request.GET['verify-password'] # verify password input
+            data.robot = self.request.GET['robot']  # Human validation input
 
-            data.dob = lib.age_check(data.dob)
-            data.password = lib.password_match(data.password, data.ver_password)
-            data.robot = lib.robot_check(data.robot)
+            data.dob = lib.age_check(data.dob)  # calls age-check method in lib & passes DOB data to validate age
+            data.password = lib.password_match(data.password, data.ver_password)  # calls password_match in lib
+            data.robot = lib.robot_check(data.robot)  # calls robot_check in lib
 
-            all = results.head + results.body + results.close
-            all_answers = all.format(**locals())
-            self.response.write(all_answers)
+            all = results.head + results.body + results.close  # adds all pieces of html file on ResultsPage class
+            all_answers = all.format(**locals())  # pushes form values into html code
+            self.response.write(all_answers)  # writes it all to the page
         else:
-            self.response.write(f.print_out())
+            self.response.write(f.print_out())  # if form is not filled out only form will display
 
 
 app = webapp2.WSGIApplication([
