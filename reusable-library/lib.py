6f284ded2a3315498688ca84f02fe5ac90f31d78
klarski <kristennl1@gmail.com>
2014-11-14 04:19:23 -0500

    add comments to lib.py

--- reusable-library/lib.py
+++ reusable-library/lib.py
@@ -1,30 +1,33 @@
-from datetime import datetime
+from datetime import datetime  # this is importing the date data from the datetime python library
 
 
 class FormCal(object):
     def __init__(self):
         pass
 
+# This method pulls the info entered into the date of birth field a checks to see if the user id 13 or older
     def age_check(self, d):
-        date_format = "%Y-%m-%d"
-        dob = datetime.strptime(d, date_format)
-        current_date = datetime.today()
+        date_format = "%Y-%m-%d" # set date format
+        dob = datetime.strptime(d, date_format)  # reformat the date format pulled in from form
+        current_date = datetime.today()  # gets the current date
         age = abs((current_date-dob).days)
-        if int(age) >= (13*365):
+        if int(age) >= (13*365):  # calclates to see if the user is old enough
             print "Yeah!, You are old enough"
             return str(d)
         else:
             return "You are not old enough"
+        # Notes: check age entered into date field and see if user is over the age of 13
 
-        # check age entered into date field and see if user is over the age of 13
+#this method checks to see if the two password fields match
     def password_match(self, p1, p2):
         print str(p1) + str(p2)
         if p1 == p2:
             return p1
         else:
             return "The passwords do not match"
-        #check to see if password entered in password field matched password entered in the verify password field
+        # Notes: check to see if password entered in password field matched password entered in the verify password field
 
+#this method preforms a simple calculation based off the image in the form that verifies a human filled it out
     def robot_check(self, r):
         if int(r) == (3+1):
             print "You look legit"
@@ -42,6 +45,7 @@ class FormData(object):
         self.__password = ''
         self.__ver_password = ''
         self.__robot = 0
+# above sets up the attributes for the fields in the form- everything is set to private for encapsulating the form info
 
     @property
     def fname(self):
