34418a1348683aff750c75462187034d2e6c100e
klarski <kristennl1@gmail.com>
2014-11-20 00:25:30 -0500

    set up data.py template for data objects

---
+++ dynamic-site/data.py
@@ -0,0 +1,62 @@
+class CoffeeTypes(object):
+    def __init__(self):
+        self._name = ""
+        self._price = 0
+        self._roast = ""
+        self._region = ""
+        self._flavors = ""
+        self._weight = 0
+
+class BrooklynBlend(CoffeeTypes):
+    def __init__(self):
+        super(CoffeeTypes, self).__init__()
+        self._name = ""
+        self._price = 0
+        self._roast = ""
+        self._region = ""
+        self._flavors = ""
+        self._weight = 0
+
+
+class PortlandBlend(CoffeeTypes):
+    def __init__(self):
+        super(CoffeeTypes, self).__init__()
+        self._name = ""
+        self._price = 0
+        self._roast = ""
+        self._region = ""
+        self._flavors = ""
+        self._weight = 0
+
+
+class AustinBlend(CoffeeTypes):
+    def __init__(self):
+        super(CoffeeTypes, self).__init__()
+        self._name = ""
+        self._price = 0
+        self._roast = ""
+        self._region = ""
+        self._flavors = ""
+        self._weight = 0
+
+
+class BostonBlend(CoffeeTypes):
+    def __init__(self):
+        super(CoffeeTypes, self).__init__()
+        self._name = ""
+        self._price = 0
+        self._roast = ""
+        self._region = ""
+        self._flavors = ""
+        self._weight = 0
+
+
+class SanFranBlend(CoffeeTypes):
+    def __init__(self):
+        super(CoffeeTypes, self).__init__()
+        self._name = ""
+        self._price = 0
+        self._roast = ""
+        self._region = ""
+        self._flavors = ""
+        self._weight = 0
