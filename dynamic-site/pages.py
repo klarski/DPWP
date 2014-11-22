9a77f668714c776762eb3f60d148aeaa861f89d8
klarski <kristennl1@gmail.com>
2014-11-22 03:43:03 -0500

    Add comments to Content Page class

--- dynamic-site/pages.py
+++ dynamic-site/pages.py
@@ -49,34 +49,38 @@ class Page(object):
         </html>
         '''
 
-# the print_out function concatenates all the peices of my html page togeather and returns the full page.
+# the print_out function concatenates all the pieces of my html page togeather and returns the full page.
     def print_out(self):
         return self.head + self.header + self.main_product + self.all_the_coffee + self.close
 
+#the ContentPage class is a subclass of the superclass Page.
 class ContentPage(Page):
     def __init__(self):
         Page.__init__(self)
 
+# this is the function that adds the html and the active button's information together- it is called in main.py
     def product_view(self, main_p):
         self.main_product = '<div class="feat-product"><img src="' + main_p.image + '" width="300"/><h1>' + main_p.name + '</h2><h3>$' + str(main_p.price) + ' for a 1lb bag</h3><p>Roast: ' + main_p.roast + '</br>Regions: ' + main_p.regions + '</br>Flavors: ' + main_p.flavors + '</p><button>Add to Cart</button></div>'
 
 
+#this is the function that accepts the data from my coffee blends array and adds it together with the html to create my buttons
     def coffee_buttons(self, a):
-        self.coffee_thumb_div = '<div id="coffee-bags"><h1>Coffee Blends</h1>'
+        self.coffee_thumb_div = '<div id="coffee-bags"><h1>Coffee Blends</h1>'  # this is just to wrap my buttons in a div for css
 
-        self.coffee_thumb1 = '<div class="product">' + '<a href="?name=' + a[0].name + '"><img src="images/thumb1.jpg" width="200"/></a><h3>' + a[0].name + " " + str(a[0].price) + '</h3></div>'
+        self.coffee_thumb1 = '<div class="product">' + '<a href="?name=' + a[0].name + '"><img src="images/thumb1.jpg" width="200"/></a><h3>' + a[0].name + " " + str(a[0].price) + '</h3></div>'  # This is creating the button for Brooklyn Blend
 
-        self.coffee_thumb2 = '<div class="product">' + '<a href="?name=' + a[1].name + '"><img src="images/thumb2.jpg" width="200"/></a><h3>' + a[1].name + " " + str(a[1].price) + '</h3></div>'
+        self.coffee_thumb2 = '<div class="product">' + '<a href="?name=' + a[1].name + '"><img src="images/thumb2.jpg" width="200"/></a><h3>' + a[1].name + " " + str(a[1].price) + '</h3></div>'  # This is creating the button for Portland Blend
 
-        self.coffee_thumb3 = '<div class="product">' + '<a href="?name=' + a[2].name + '"><img src="images/thumb3.jpg" width="200"/></a><h3>' + a[2].name + " " + str(a[2].price) + '</h3></div>'
+        self.coffee_thumb3 = '<div class="product">' + '<a href="?name=' + a[2].name + '"><img src="images/thumb3.jpg" width="200"/></a><h3>' + a[2].name + " " + str(a[2].price) + '</h3></div>'   # This is creating the button for Austin Blend
 
-        self.coffee_thumb4 = '<div class="product">' + '<a href="?name=' + a[3].name + '"><img src="images/thumb4.jpg" width="200"/></a><h3>' + a[3].name + " " + str(a[3].price) + '</h3></div>'
+        self.coffee_thumb4 = '<div class="product">' + '<a href="?name=' + a[3].name + '"><img src="images/thumb4.jpg" width="200"/></a><h3>' + a[3].name + " " + str(a[3].price) + '</h3></div>'  # This is creating the button for Boston Blend
 
-        self.coffee_thumb5 = '<div class="product">' + '<a href="?name=' + a[4].name + '"><img src="images/thumb5.jpg" width="200"/></a><h3>' + a[4].name + " " + str(a[4].price) + '</h3></div>'
+        self.coffee_thumb5 = '<div class="product">' + '<a href="?name=' + a[4].name + '"><img src="images/thumb5.jpg" width="200"/></a><h3>' + a[4].name + " " + str(a[4].price) + '</h3></div>'   # This is creating the button for SanFran Blend
 
-        self.coffee_thumb_div_close = '</div>'
+        self.coffee_thumb_div_close = '</div>'  # closing my div
 
-        self.all_the_coffee += self.coffee_thumb_div + self.coffee_thumb1 + self.coffee_thumb2 + self.coffee_thumb3 + self.coffee_thumb4 + self.coffee_thumb5 + self.coffee_thumb_div_close
+        self.all_the_coffee += self.coffee_thumb_div + self.coffee_thumb1 + self.coffee_thumb2 + self.coffee_thumb3 + self.coffee_thumb4 + self.coffee_thumb5 + self.coffee_thumb_div_close  #this is adding each button together
 
+# the print_out function concatenates all the pieces of my html page together and returns the full page.
     def print_out(self):
         return self.head + self.header + self.main_product + self.all_the_coffee + self.close
